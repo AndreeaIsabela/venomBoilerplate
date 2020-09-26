69a8ab585778357b67d2e409880ebde78e8104de
AndreeaIsabela <bala.andreea@yahoo.com>
2020-09-26 21:24:41 +0300

    [master]  Fix authenticate bug.

--- src/client/src/views/login/login.ts
+++ src/client/src/views/login/login.ts
@@ -117,7 +117,7 @@ export default class Login extends Vue {
     } else {
       this.emailError.status = '';
     }
-    return email.length;
+    return !!email.length;
   }
 
   private checkPassword(password: string) {
